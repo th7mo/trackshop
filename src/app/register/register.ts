877e63d8a36bb17b866c6c19419c00ad296125d3
th7mo <thimo@th7mo.com>
2025-12-04 20:19:43 +0100

    feat: add icon and styling for pages

--- src/app/register/register.ts
+++ src/app/register/register.ts
@@ -1,7 +1,5 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CustomerService } from '../customer/customer.service';
-import { Router } from '@angular/router';
 import { RegisterForm } from '../register-form/register-form';
 
 @Component({
